046425557ce47e42e28ea9200b86d46571d756d7
Alexander Weigl <weigl@kit.edu>
2025-08-19 08:16:06 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,133 @@
+import org.gradle.kotlin.dsl.assign
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "2.2.0"
+    id("org.jetbrains.dokka") version "2.0.0"
+    `java-library`
+    `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+}
+
+group = "io.github.wadoon.key"
+version = "1.0-SNAPSHOT"
+val keyVersion = System.getenv("KEY_VERSION") ?: "2.12.4-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://central.sonatype.com/repository/maven-snapshots")}
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    //implementation(kotlin("serialization"))
+
+    implementation("org.key-project:key.core:${keyVersion}")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+
+    testImplementation(kotlin("test"))
+    testImplementation("com.google.truth:truth:1.4.4")
+}
+
+tasks.withType<KotlinCompile> {
+    compilerOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = JvmTarget.JVM_21
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    reports.html.required.set(false)
+    reports.junitXml.required.set(true)
+    testLogging {
+        events("skipped", "failed")
+        showExceptions = true
+    }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.withType<Javadoc> {
+    isFailOnError = false
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+//            from(components["kotlin"])
+
+            repositories {
+                maven {
+                    name = "folder"
+                    url = uri("$rootDir/release")
+                }
+            }
+
+
+            pom {
+                name = "key-ci-tool"
+                description = project.description
+                url = "https://github.com/wadoon/key-citool"
+                licenses {
+                    license {
+                        name = "GNU General Public License (GPL), Version 2"
+                        url = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.html"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "wadoon"
+                        name = "Alexander Weigl"
+                        email = "weigl@kit.edu"
+                    }
+                }
+                scm {
+                    connection = "git@github.com:wadoon/key-citool.git"
+                    url = "https://github.com/wadoon/key-citool"
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        create("central") {
+            nexusUrl = uri("https://ossrh-staging-api.central.sonatype.com/service/local/")
+            snapshotRepositoryUrl = uri("https://central.sonatype.com/repository/maven-snapshots/")
+
+            stagingProfileId.set("org.key-project")
+            val user: String = project.properties.getOrDefault("ossrhUsername", "").toString()
+            val pwd: String = project.properties.getOrDefault("ossrhPassword", "").toString()
+
+            username.set(user)
+            password.set(pwd)
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
+}
